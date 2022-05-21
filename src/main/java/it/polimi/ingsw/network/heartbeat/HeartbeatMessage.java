17dcfe083e1e43a188be3916f485991bb157c18a
teomormi <100494143+teomormi@users.noreply.github.com>
2022-05-21 19:31:47 +0200

    Fixed heartbeat message and sender

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartbeatMessage.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartbeatMessage.java
@@ -3,24 +3,34 @@ package it.polimi.ingsw.network.heartbeat;
 import it.polimi.ingsw.client.View;
 import it.polimi.ingsw.network.CVMessage;
 import it.polimi.ingsw.network.Message;
+import it.polimi.ingsw.network.SYSMessage;
 import it.polimi.ingsw.network.VCMessage;
+import it.polimi.ingsw.server.ServerMain;
 import it.polimi.ingsw.server.VirtualView;
 import it.polimi.ingsw.util.MessageType;
 
-public class HeartbeatMessage implements CVMessage, VCMessage, Message {
-    private final MessageType messageType = MessageType.SYS;
+import java.io.Serializable;
+
+public class HeartbeatMessage implements CVMessage, VCMessage, Serializable {
+    private final MessageType messageType;
+    public HeartbeatMessage(MessageType messageType) {
+        this.messageType = messageType;
+    }
 
     @Override
     public MessageType getType() {
         return messageType;
     }
 
+
     @Override
     public void execute(View view) {
+
     }
 
     @Override
     public void execute(VirtualView virtualView) {
+
     }
 }
 
--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartbeatSender.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartbeatSender.java
@@ -2,11 +2,14 @@ package it.polimi.ingsw.network.heartbeat;
 
 import it.polimi.ingsw.network.NetworkHandler;
 import it.polimi.ingsw.util.Configurator;
+import it.polimi.ingsw.util.MessageType;
 
 public class HeartbeatSender extends Thread{
     private final NetworkHandler networkHandler;
+    private final MessageType messageType;
 
-    public HeartbeatSender(NetworkHandler networkHandler) {
+    public HeartbeatSender(NetworkHandler networkHandler,MessageType messageType) {
+        this.messageType = messageType;
         this.networkHandler = networkHandler;
     }
 
@@ -14,6 +17,7 @@ public class HeartbeatSender extends Thread{
     public void run() {
         while(networkHandler.isConnected()){
             try {
+                networkHandler.send(new HeartbeatMessage(messageType));
                 Thread.sleep(Configurator.getHeartbeatInterval());
             } catch (InterruptedException ignored) {
             }
