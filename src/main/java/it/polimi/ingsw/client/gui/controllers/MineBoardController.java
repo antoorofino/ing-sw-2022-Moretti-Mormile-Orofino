22aa5cd4935b9d79b7e39891279520cc1296ecff
Lomoretti <100613289+Lomoretti@users.noreply.github.com>
2022-06-22 11:44:00 +0200

    Added pieces for student and teacher on board GUI

--- src/main/java/it/polimi/ingsw/client/gui/controllers/MineBoardController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/MineBoardController.java
@@ -4,14 +4,35 @@ package it.polimi.ingsw.client.gui.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+
 public class MineBoardController extends SceneController{
     @FXML
-    GridPane green1;
+    GridPane green1, green2, green3, green4, green5, green6, green7, green8, green9, green10;
+    ArrayList<GridPane> studentsGreenRoom = new ArrayList<GridPane>();
 
+    @FXML
+    public void initialize() {
+        buildArrayList();
+    }
     public void moveToIsland(){
         green1.getStyleClass().clear();
     }
     public void moveToRoom(){
         green1.getStyleClass().add("student-green-background");
+    }
+
+    private void buildArrayList(){
+        /*--GREEN--*/
+        studentsGreenRoom.add(green1);
+        studentsGreenRoom.add(green2);
+        studentsGreenRoom.add(green3);
+        studentsGreenRoom.add(green4);
+        studentsGreenRoom.add(green5);
+        studentsGreenRoom.add(green6);
+        studentsGreenRoom.add(green7);
+        studentsGreenRoom.add(green8);
+        studentsGreenRoom.add(green9);
+        studentsGreenRoom.add(green10);
     }
 }
--- src/main/java/it/polimi/ingsw/client/gui/utils/GUISwitcher.java
+++ src/main/java/it/polimi/ingsw/client/gui/utils/GUISwitcher.java
@@ -86,6 +86,6 @@ public class GUISwitcher {
     }
 
     public void setDefaultController() {
-        changeController(homeSceneController);
+        changeController(mineBoardController);
     }
 }
